3d6f95bd43077139c0a399ad022367d302eaf1a8
Logan Lasiter <lasiterlogan@gmail.com>
2017-09-26 11:56:49 -0700

    fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 function toggleMenu() {
-	var nav = document.getElementById("responsive");
-	var links = document.getElementById('links');
-	if(nav.style.display == 'flex'){
-		nav.classList.add('shrink');
-		setTimeout(function(){ links.style.display = 'none'; }, 300);
-		setTimeout(function(){ nav.style.display = 'none'; }, 500);
-		return;
-	}
-	nav.classList.remove('shrink');
-	nav.classList.add('expand');
-	nav.style.display = 'flex';
-	links.style.display = 'flex';
+	var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
